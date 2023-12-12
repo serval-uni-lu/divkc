e5ae5668735ff2b788ab0071db53acaebf25754a
zyno1 <olivierzeyen@gmail.com>
2023-12-12 15:50:24 +0100

    pagmo splitter mod

--- pagmo_splitter/src/main.cpp
+++ pagmo_splitter/src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <fstream>
 #include <limits>
+#include <chrono>
+#include <algorithm>
 
 #include <cmath>
 
@@ -45,7 +48,7 @@ struct Prob {
 
                 if(tmp != 0 && tmp != cl.size()) {
                     //res += 1; // some non-zero value
-                    res += pow(2, 5 - cl.size());
+                    res += pow(2, 4 - cl.size());
                 }
             }
         }
@@ -62,6 +65,9 @@ struct Prob {
 };
 
 int main(int argc, char** argv) {
+    auto ts = std::chrono::steady_clock::now();
+
+    std::string const path(argv[1]);
     CNF cnf(argv[1]);
 
     Prob prob;
@@ -69,25 +75,55 @@ int main(int argc, char** argv) {
 
     problem p{prob};
 
-    algorithm algo{sga(1000)};
+    //algorithm algo{sga(1000)};
 
-    //auto g = gaco{1000};
-    //algorithm algo{g};
+    auto g = gaco{10000};
+    algorithm algo{g};
 
-    population pop{p, 10000};
+    population pop{p, 1000};
 
     pop = algo.evolve(pop);
 
-    std::cout << p ;
+    //std::cout << p ;
     int64_t sum = 0;
     for(int i = 0; i < cnf.nb_vars(); i++) {
         std::cout << "v " << (i + 1) << " " << pop.champion_x()[i] << "\n";
         sum += pop.champion_x()[i];
     }
+
+    std::cout << "c c 2\n";
     std::cout << "c p 0 " << cnf.nb_vars() - sum << "\n";
     std::cout << "c p 1 " << sum << "\n";
 
-    std::cout << "s2 " << pop.champion_f()[0] << "\n";
+    for(int64_t i = 0; i < 2; i++) {
+        std::vector<int> ids;
+
+        for(int64_t j = 0; j < cnf.nb_clauses(); j++) {
+            if(cnf.is_active(j)) {
+                auto const& cl = cnf.clause(i);
+
+                bool toggle = std::any_of(cl.begin(), cl.end(), [&](auto const& l) {
+                        return pop.champion_x()[Variable(l).get()] != i;
+                        });
+
+                if(toggle) {
+                    cnf.set_active(j, false);
+                    ids.push_back(j);
+                }
+            }
+        }
+
+        std::string tmp_path = path + ".split" + std::to_string(i);
+        std::ofstream out(tmp_path);
+        out << cnf;
+        out.close();
+        std::cout << "p " << i << " " << "split" << i << "\n";
+
+        for(int j : ids) {
+            cnf.set_active(j, true);
+        }
+    }
+
 
     int64_t res = 0;
     for(int64_t i = 0; i < cnf.nb_clauses(); i++) {
@@ -107,7 +143,11 @@ int main(int argc, char** argv) {
         }
     }
 
+    std::cout << "s2 " << pop.champion_f()[0] << "\n";
     std::cout << "s " << res << "\n";
+
+    std::chrono::duration<double> dur = std::chrono::steady_clock::now() - ts;
+    std::cout << "c t " << dur.count() << "\n";
 
     return 0;
 }
