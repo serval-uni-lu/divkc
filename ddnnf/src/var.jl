9fdfe7db4ae574aa35b5a78cecc343ead9f05b0c
zyno1 <olivierzeyen@gmail.com>
2025-01-22 15:35:55 +0100

    base ddnnf parser

---
+++ ddnnf/src/var.jl
@@ -0,0 +1,19 @@
+struct Lit
+    x :: Int32
+end
+
+struct Var
+    x :: Int32
+end
+
+mkVar(x :: Integer) = Var(Int32(abs(x) - 1))
+mkVar(x :: Lit) = Var(x.x >> 1)
+
+mkLit(x :: Integer) = mkLit(mkVar(x), x < 0)
+mkLit(x :: Var, s :: Bool) = Lit(x.x << 1 | (s ? 1 : 0))
+
+toIndex(x :: Var) = x.x + 1
+toIndex(x :: Lit) = x.x + 1
+
+mkReadable(x :: Var) = x.x + 1
+mkReadable(x :: Lit) = ((x.x >> 1) + 1) * (x.x & 1 == 1 ? -1 : 1)
