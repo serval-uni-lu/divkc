1d3a00cd97db2936dc972860236155ed8e9cefe4
zyno1 <olivierzeyen@gmail.com>
2025-02-21 15:36:01 +0100

    split mod

--- pagmo_splitter/src/main.cpp
+++ pagmo_splitter/src/main.cpp
@@ -29,7 +29,7 @@ struct Prob {
             res += c;
         }
 
-        if(res == 0 || res == v.size()) {
+        if(res == 0 || res >= 0.9 * v.size()) {
             return {std::numeric_limits<double>::max()};
         }
 
@@ -53,7 +53,8 @@ struct Prob {
                 }
             }
         }
-        return {static_cast<double>(-1 * res) / nb_v};
+        // return {static_cast<double>(-1 * res) / nb_v};
+        return {static_cast<double>(-1 * res)};
     }
 
     std::pair<vector_double, vector_double> get_bounds() const {
@@ -66,8 +67,6 @@ struct Prob {
 };
 
 int main(int argc, char** argv) {
-    auto ts = std::chrono::steady_clock::now();
-
     if(argc != 4) {
         std::cout << "usage: splitter <cnf> <generations> <population>\nexiting\n";
         return 0;
@@ -93,127 +92,15 @@ int main(int argc, char** argv) {
 
     pop = algo.evolve(pop);
 
-    //std::cout << p ;
-    int64_t sum = 0;
-
     {
-        std::string tmp_path = path + ".fpv";
-        std::ofstream out(tmp_path);
-        for(int i = 0; i < cnf.nb_vars(); i++) {
-            std::cout << "v " << (i + 1) << " " << pop.champion_x()[i] << "\n";
-            sum += pop.champion_x()[i];
-
-            if(pop.champion_x()[i] == 1) {
-                out << (i + 1) << "\n";
-            }
-        }
-        out.close();
-    }
-
-    std::cout << "c c 2\n";
-    std::cout << "c p 0 " << cnf.nb_vars() - sum << "\n";
-    std::cout << "c p 1 " << sum << "\n";
-
-    std::vector<int> ids;
-
-    // // comm 1 (projection)
-    // for(int64_t j = 0; j < cnf.nb_clauses(); j++) {
-    //     if(cnf.is_active(j)) {
-    //         auto const& cl = cnf.clause(j);
-
-    //         bool toggle = false;
-    //         for(auto const & l : cl) {
-    //             toggle = toggle || pop.champion_x()[Variable(l).get()] != 1;
-    //         }
-
-    //         if(toggle) {
-    //             cnf.set_active(j, false);
-    //             ids.push_back(j);
-    //         }
-    //     }
-    // }
-
-    // {
-    // std::string tmp_path = path + ".split1";
-    // std::ofstream out(tmp_path);
-    // out << cnf;
-    // out.close();
-    // std::cout << "p 1 split1\n";
-    // }
-    {
-        std::string tmp_path = path + ".prj";
-        std::ofstream out(tmp_path);
-
-        out << "c ind ";
+        std::cout << "c p show ";
         for(int i = 0; i < cnf.nb_vars(); i++) {
             if(pop.champion_x()[i] == 1) {
-                out << (i + 1) << " ";
+                std::cout << (i + 1) << " ";
             }
         }
-        out << "0\n";
-        out << cnf;
-        out.close();
-        std::cout << "p 1 split1\n";
+        std::cout << "0";
     }
-
-    // for(int j : ids) {
-    //     cnf.set_active(j, true);
-    // }
-    // ids.clear();
-
-    // comm 0 (upper bound)
-    for(int64_t j = 0; j < cnf.nb_clauses(); j++) {
-        if(cnf.is_active(j)) {
-            auto const& cl = cnf.clause(j);
-
-            bool toggle = false;
-            for(auto const & l : cl) {
-                toggle = toggle || pop.champion_x()[Variable(l).get()] != 1;
-            }
-
-            if(!toggle) {
-                cnf.set_active(j, false);
-                ids.push_back(j);
-            }
-        }
-    }
-
-    {
-    std::string tmp_path = path + ".up";
-    std::ofstream out(tmp_path);
-    out << cnf;
-    out.close();
-    std::cout << "p 0 split0\n";
-    }
-
-    for(int j : ids) {
-        cnf.set_active(j, true);
-    }
-    ids.clear();
-
-    int64_t res = 0;
-    for(int64_t i = 0; i < cnf.nb_clauses(); i++) {
-        if(cnf.is_active(i)) {
-            int64_t tmp = 0;
-            auto const& cl = cnf.clause(i);
-
-            for(auto const& l : cl) {
-                Variable vl(l);
-
-                tmp += pop.champion_x()[vl.get()];
-            }
-
-            if(tmp != 0 && tmp != cl.size()) {
-                res += 1;
-            }
-        }
-    }
-
-    std::cout << "s2 " << pop.champion_f()[0] << "\n";
-    std::cout << "s " << res << "\n";
-
-    std::chrono::duration<double> dur = std::chrono::steady_clock::now() - ts;
-    std::cout << "c t " << dur.count() << "\n";
 
     return 0;
 }
