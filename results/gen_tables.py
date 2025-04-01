3c2f16d57d962dc9c66d159df733073d24f50114
zyno1 <olivierzeyen@gmail.com>
2025-04-01 16:08:39 +0200

    results

---
+++ results/gen_tables.py
@@ -0,0 +1,146 @@
+import pandas as pd
+import numpy as np
+from mpmath import mp
+
+mc = pd.read_csv("cnf.mc.csv", skipinitialspace = True, index_col = 'file')
+clt = pd.read_csv("cnf.clt.csv", skipinitialspace = True, index_col = 'file')
+# glmc = pd.read_csv("cnf.lmc.csv", skipinitialspace = True, index_col = 'file')
+bounds = pd.read_csv("cnf.bounds.csv", skipinitialspace = True, index_col = 'file')
+total = pd.read_csv("total.csv", skipinitialspace = True)
+
+mc.dropna(inplace = True)
+clt.dropna(inplace = True)
+# glmc.dropna(inplace = True)
+bounds.dropna(inplace = True)
+total.dropna(inplace = True)
+
+d = clt.join(mc, on = 'file')
+# d = d.join(glmc, on = 'file')
+d = d.join(bounds, on = 'file')
+d.dropna(inplace = True)
+
+for x in total.index:
+    sub = total['folder'][x]
+    nbf = total['nbf'][x]
+    vsub = total['map'][x]
+
+    ldf = d[d.index.str.contains(sub)]
+    lmc = mc[mc.index.str.contains(sub)]
+    lgb = clt[clt.index.str.contains(sub)]
+
+    nmc = len(lmc)
+
+    slgb = set(lgb.index)
+    slmc = set(lmc.index)
+
+    k = len(slgb - slmc)
+
+    nd4 = len(slmc)
+    nt = len(slgb)
+    nboth = len(slmc.intersection(slgb))
+
+
+    if k > 0:
+        k = "\\textbf{" + str(k) + "}"
+    # print(f"{vsub} & {nbf} & {len(lmc)} & {k} & {nlow:5.3f} & {nhigh:5.3f} & {nboth:5.3f} & {ntlow:5.3f} \\\\")
+    print(f"{vsub} & {nbf} & {nboth} & {nbf - len(lmc)} & {k} \\\\")
+
+print("------------------------------------------------------------")
+for x in total.index:
+    sub = total['folder'][x]
+    nbf = total['nbf'][x]
+    vsub = total['map'][x]
+
+    ldf = d[d.index.str.contains(sub)]
+    lmc = mc[mc.index.str.contains(sub)]
+    lgb = clt[clt.index.str.contains(sub)]
+
+    nmc = len(lmc)
+
+    slgb = set(lgb.index)
+    slmc = set(lmc.index)
+
+    k = len(slgb - slmc)
+
+    nlow = 0
+    nhigh = 0
+    nboth = 0
+    nb = 0
+
+    ntlow = 0
+    nthigh = 0
+
+    for y in ldf.index:
+        ntlow += (mp.mpf(ldf.mc[y]) >= mp.mpf(ldf.low[y]))
+        nthigh += (mp.mpf(ldf.mc[y]) <= mp.mpf(ldf.high[y]))
+
+        nlow += mp.mpf(ldf.Yl[y]) <= mp.mpf(ldf.mc[y])
+        nhigh += mp.mpf(ldf.Yh[y]) >= mp.mpf(ldf.mc[y])
+        nboth += (mp.mpf(ldf.Yh[y]) >= mp.mpf(ldf.mc[y])) and (mp.mpf(ldf.Yl[y]) <= mp.mpf(ldf.mc[y]))
+        nb += 1
+
+
+    if nb > 0:
+        nlow /= nb
+        nhigh /= nb
+        nboth /= nb
+
+        ntlow /= nb
+        nthigh /= nb
+
+        if k > 0:
+            k = "\\textbf{" + str(k) + "}"
+        # print(f"{vsub} & {nbf} & {len(lmc)} & {k} & {nlow:5.3f} & {nhigh:5.3f} & {nboth:5.3f} & {ntlow:5.3f} \\\\")
+        print(f"{vsub} & {nb} & {nlow:5.3f} & {nhigh:5.3f} & {nboth:5.3f} & {ntlow:5.3f} & {nthigh:5.3f} \\\\")
+    else:
+        print(f"{vsub} & {nb} & & & & & \\\\")
+
+print("------------------------------------------------------------")
+
+# d = clt.join(bounds, on = 'file')
+# d.dropna(inplace = True)
+
+for x in total.index:
+    sub = total['folder'][x]
+    nbf = total['nbf'][x]
+    vsub = total['map'][x]
+
+    ldf = d[d.index.str.contains(sub)]
+    lmc = mc[mc.index.str.contains(sub)]
+    lgb = clt[clt.index.str.contains(sub)]
+
+    nlow = 0
+    nhigh = 0
+    nboth = 0
+    nbothc = 0
+    # nboth = 0
+    # nlvl = 0
+    # ntlow = 0
+    nb = 0
+
+    for y in ldf.index:
+        nlow += (mp.mpf(ldf.Yl[y]) >= mp.mpf(ldf.low[y])) and (mp.mpf(ldf.Yl[y]) <= mp.mpf(ldf.mc[y]))
+        nhigh += mp.mpf(ldf.Yh[y]) <= mp.mpf(ldf.high[y]) and (mp.mpf(ldf.Yh[y]) >= mp.mpf(ldf.mc[y]))
+        nboth += (mp.mpf(ldf.Yl[y]) >= mp.mpf(ldf.low[y])) and (mp.mpf(ldf.Yh[y]) <= mp.mpf(ldf.high[y]))
+        nbothc += (mp.mpf(ldf.Yl[y]) >= mp.mpf(ldf.low[y])) and (mp.mpf(ldf.Yh[y]) <= mp.mpf(ldf.high[y])) and (mp.mpf(ldf.Yl[y]) <= mp.mpf(ldf.mc[y])) and (mp.mpf(ldf.Yh[y]) >= mp.mpf(ldf.mc[y]))
+        # ntlow += mp.mpf(ldf.lmc[y]) >= mp.mpf(ldf.low[y])
+        # nlvl += (mp.mpf(ldf.lmc[y]) >= mp.mpf(ldf.Yl[y])) and (mp.mpf(ldf.lmc[y]) >= mp.mpf(ldf.low[y])) and (mp.mpf(ldf.mc[y]) >= mp.mpf(ldf.lmc[y]))
+        nb += 1
+
+
+    if nb > 0:
+        nlow /= nb
+        nhigh /= nb
+        # ntlow /= nb
+        # nlvl /= nb
+        # nboth /= nb
+        nbothc /= nb
+
+        # print(f"{vsub} & {nb} & {nlow:5.3f} & {nhigh:5.3f} & {nboth:5.3f} & {nbothc:5.3f} \\\\")
+        print(f"{vsub} & {nb} & {nlow:5.3f} & {nhigh:5.3f} & {nbothc:5.3f} \\\\")
+
+        # print(f"{vsub} & {nbf} & {len(lmc)} & {k} & {nlow:5.3f} & {nhigh:5.3f} & {nboth:5.3f} & {ntlow:5.3f} \\\\")
+        # print(f"{vsub} & {nbf} & {len(lmc)} & {k} & {nlow:5.3f} & {nhigh:5.3f} & {nboth:5.3f} \\\\")
+    else:
+        # print(f"{vsub} & {nbf} & {len(lmc)} & {k} & & & \\\\")
+        print(f"{vsub} & {nb} & & & \\\\")
