122c72694d1f989846e87ed5c6710c06bbbb66de
zyno1 <olivierzeyen@gmail.com>
2025-04-01 15:58:45 +0200

    cleanup

--- bench_select/get.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-function run {
-    cp --parents "$HOME/Documents/Work/experiments/$1" "./fd/"
-}
-export -f run
-
-cat "$1" | cut -d ',' -f 1 | grep -v 'file' | parallel -n 1 -P 1 run
--- bench_select/smp_r.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function run {
-    # cp --parents "$HOME/Documents/Work/experiments/$1" "./f/"
-    ./smp_rename "$1" > "$1.smp"
-}
-export -f run
-
-find "$1" -name "*.cnf" | parallel -n 1 -P 4 run
