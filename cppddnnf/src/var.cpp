f775403c78b2e93ece09a162ade17b8f679ac0a8
zyno1 <olivierzeyen@gmail.com>
2026-02-17 11:18:59 +0100

    cleanup

--- cppddnnf/src/var.cpp
+++ cppddnnf/src/var.cpp
@@ -9,33 +9,59 @@ Literal::Literal (Variable v, int sign) : l((v.get() << 1) | (sign < 0 ? 1 : 0))
 Literal::Literal (int i) : l(((std::abs(i) - 1) << 1) ^ (i < 0 ? 1 : 0)) {
 }
 
-bool Literal::operator == (Literal p) const {
-    return l == p.get();
-}
-
-bool Literal::operator != (Literal p) const {
-    return l != p.get();
-}
-
-// '<' makes p, ~p adjacent in the ordering.
-bool Literal::operator <  (Literal p) const {
-    return l < p.get();
-}  
-
 Variable::Variable(int i) : v(std::abs(i) - 1) {
 }
 
 Variable::Variable(Literal l) : v(l.get() >> 1) {
 }
 
-bool Variable::operator == (Variable p) const {
+bool Variable::operator == (Variable const& p) const {
     return v == p.v;
 }
 
-bool Variable::operator != (Variable p) const {
+bool Variable::operator != (Variable const& p) const {
     return v != p.v;
 }
 
-bool Variable::operator <  (Variable p) const {
+bool Variable::operator <  (Variable const& p) const {
     return v < p.v;
+}
+
+LitSet::LitSet(std::size_t const nb_lits) : set(nb_lits, 0) {
+}
+
+LitSet::LitSet() : set(0, 0) {
+}
+
+void LitSet::insert(Literal const& l) {
+    if(l.get() >= static_cast<int>(set.size())) {
+        set.resize(std::max(l.get(), (~l).get()) + 1);
+    }
+    set.set(l.get(), true);
+}
+
+void LitSet::remove(Literal const& l) {
+    if(l.get() < static_cast<int>(set.size())) {
+        set.set(l.get(), false);
+    }
+}
+
+void LitSet::reset() {
+    set.reset();
+}
+
+bool LitSet::contains(Literal const& l) const {
+    return l.get() < static_cast<int>(set.size()) && set.test(l.get());
+}
+
+void LitSet::resize(std::size_t const nb_lits) {
+    set.resize(nb_lits);
+}
+
+bool operator<(LitSet const& l, LitSet const& r) {
+    return l.set < r.set;
+}
+
+bool operator==(LitSet const& l, LitSet const& r) {
+    return l.set == r.set;
 }
--- cppddnnf/src/var.hpp
+++ cppddnnf/src/var.hpp
@@ -1,7 +1,9 @@
 #ifndef VAR_HPP
 #define VAR_HPP
 
-#include<iostream>
+#include <iostream>
+
+#include <boost/dynamic_bitset.hpp>
 
 struct Literal;
 struct Variable;
@@ -61,20 +63,52 @@ struct Literal {
      */
     inline int get() const { return l; }
 
-    bool operator == (Literal p) const;
-    bool operator != (Literal p) const;
-
-    /**
-     * \returns
-     * true if this.get() < p.get().
-     *
-     * as an example we have in DIMACS format:
-     * 1 < -1 < 2 < -2 < 3 < -3 < ...
-     */
-    bool operator <  (Literal p) const; // '<' makes p, ~p adjacent in the ordering.  
-
     friend inline Literal operator ~ (Literal p);
 };
+
+namespace std {
+    template<>
+    struct hash<Literal> {
+        std::size_t operator()(Literal const& l) const noexcept {
+            return std::hash<int>()(l.get());
+        }
+    };
+}
+
+/**
+ * \returns
+ * true if a.get() < b.get().
+ *
+ * as an example we have in DIMACS format:
+ * 1 < -1 < 2 < -2 < 3 < -3 < ...
+ */
+inline bool operator<(Literal const& a, Literal const& b) {
+    return a.get() < b.get();
+}
+
+inline bool operator>(Literal const& a, Literal const& b) {
+    return a.get() > b.get();
+}
+
+inline bool operator==(Literal const& a, Literal const& b) {
+    return a.get() == b.get();
+}
+
+inline bool operator>=(Literal const& a, Literal const& b) {
+    return a > b || a == b;
+}
+
+/**
+ * \returns
+ * true if a.get() <= b.get().
+ */
+inline bool operator<=(Literal const& a, Literal const& b) {
+    return a < b || a == b;
+}
+
+inline bool operator!=(Literal const& a, Literal const& b) {
+    return a.get() != b.get();
+}
 
 /**
  * \returns The logical negation of this literal
@@ -125,33 +159,96 @@ struct Variable {
     Variable& operator= (Variable && p) = default;
 
     /**
-     * \returns The internal representation of this variable for use
-     * as an array index.
+     * \returns A positive literal representation of this variable in DIMACS format
      */
     inline int to_int() const { return v + 1; }
 
     /**
-     * \returns A positive literal representation of this variable in DIMACS format
+     * \returns The internal representation of this variable for use
+     * as an array index.
      */
     inline int get() const { return v; }
 
-    bool operator == (Variable p) const;
-    bool operator != (Variable p) const;
+    bool operator == (Variable const& p) const;
+    bool operator != (Variable const& p) const;
 
     /**
      * \returns
      * true if this.to_int() < p.to_int()
      */
-    bool operator <  (Variable p) const;
+    bool operator <  (Variable const& p) const;
 };
 
-/**
- * \brief Prints the variable in DIMACS format to the stream
- * \relates Variable
- */
-inline std::ostream & operator<<(std::ostream & out, Variable const& v) {
-    out << v.to_int();
-    return out;
+namespace std {
+    template<>
+    struct hash<Variable> {
+        std::size_t operator()(Variable const& v) const noexcept {
+            return std::hash<int>()(v.get());
+        }
+    };
 }
+
+/**
+ * \brief A set of literals using bitsets for fast insertion and lookup.
+ * \details
+ * This is not a multiset, therefore, elements have at most one occurence
+ * in the set.
+ */
+struct LitSet {
+    private:
+    boost::dynamic_bitset<> set;
+
+    public:
+    /**
+     * \brief Creates an empty set with a preallocated bitset.
+     */
+    LitSet(std::size_t const nb_lits);
+    LitSet();
+
+    LitSet(LitSet const& v) = default;
+    LitSet(LitSet && v) = default;
+
+    LitSet& operator= (LitSet const& p) = default;
+    LitSet& operator= (LitSet && p) = default;
+
+    /**
+     * \brief Insert a literal into the set.
+     */
+    void insert(Literal const& l);
+    /**
+     * \brief Remove a literal from the set.
+     */
+    void remove(Literal const& l);
+    /**
+     * \brief Remove every literal from the set.
+     */
+    void reset();
+
+    /**
+     * \returns true if the literal is in the set, false otherwise.
+     */
+    bool contains(Literal const& l) const;
+
+    /**
+     * \returns The number of literals in the set.
+     */
+    inline std::size_t count() const { return set.count(); }
+
+    /**
+     * \brief Resizes the bitset to accomodate literals that may have been outside of the range.
+     * \details
+     * If 2*nb_lits is smaller than the current size of the bitset (set_size()),
+     * then information may be lost.
+     */
+    void resize(std::size_t const nb_lits);
+
+    /**
+     * \returns The size of the underlying bitset.
+     */
+    inline std::size_t set_size() const { return set.size(); }
+
+    friend bool operator == (LitSet const& a, LitSet const& b);
+    friend bool operator<(LitSet const& l, LitSet const& r);
+};
 
 #endif
